9b47eea6c54286820551a59cf9dd04e9bd8891e4
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-06 15:52:32 +0100

    ADD: style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,8 +3,8 @@
 export default {
   data() {
     return {
-    //     gender: [uomo, donna, bambino],
-    //     menu: [profilo, preferiti, carrello],
+        // gender: [uomo, donna, bambino],
+        // menu: [profilo, preferiti, carrello],
     }
   },
 
@@ -22,21 +22,21 @@ export default {
         <!--gender / logo / icons-->
         <nav class="container">
             <div class="row">
-                <div class="col-2">
-                    <ul class="row justify-around">
+                <div class="menu gender-section">
+                    <ul class="row gap-2">
                         <li ><a class="gender" href="#">Uomo</a></li>
                         <li ><a class="gender" href="#">Donna</a></li>
                         <li ><a class="gender" href="#">Bambino</a></li>
                     </ul>
                 </div>
                 <div class="col-8">
-                    <div class="row justify-center">
-                        <img class="logo" src="./img/boolean-logo.png" alt="logo">
+                    <div class="">
+                        <img class="logo" src="/img/boolean-logo.png" alt="logo">
                     </div>
                 </div>
-                <div class="col-2">
-                    <ul class="row justify-around">
-                        <li ><a class="icons" href="#">prifilo</a></li>
+                <div class="menu profile-section">
+                    <ul class="row gap-2">
+                        <li ><a class="icons" href="#">profilo</a></li>
                         <li ><a class="icons" href="#">preferiti</a></li>
                         <li ><a class="icons" href="#">carrello</a></li>
                     </ul>
@@ -48,5 +48,39 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.header-page {
+    background-color: $header-color;
+    color: $header-text-color;
+    padding: 15px;
+    width: 100%;  
+    position: fixed; 
+    z-index: 100;
+    
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .menu {
+        font-size: 20px;
+    }
+    .gap-2 {
+        gap: 10px;
+    }
+
+    .logo {
+        width: 150px;
+    }
+}
+
+
+
+
+
+
 
 </style>
